789156136fbd808f8a7fa5c413231c4ae860553b
Duvan Reyes <duvan.reyesgonzalez@unicolombo.edu.co>
2024-04-21 16:04:57 -0500

    SUNDAY

---
+++ GUI26/src/gui26/GUI26.java
@@ -0,0 +1,14 @@
+
+package gui26;
+
+
+public class GUI26 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
